e8eeafa930971a38414a552ba893482a5ba1b825
daniel-nis <72157039+daniel-nis@users.noreply.github.com>
2024-04-02 22:18:05 -0700

    hopefully fixed post error on vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,39 +1,11 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     domains: ["qliwrxwpocxqxoayomeb.supabase.co"],
-//   },
-//   output: "standalone",
-//   serverRuntimeConfig: {
-//     functionTimeout: 60 * 1000, // 60 seconds
-//   },
-// };
-
-// export default nextConfig;
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
   images: {
     domains: ["qliwrxwpocxqxoayomeb.supabase.co"],
   },
+  output: "standalone",
   serverRuntimeConfig: {
     functionTimeout: 60 * 1000, // 60 seconds
-  },
-  reactStrictMode: true,
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.js$/,
-      use: [
-        {
-          loader: "next-disable-next-client-script-loader",
-          options: {
-            exclude: /node_modules/,
-          },
-        },
-      ],
-    });
-    return config;
   },
 };
 
